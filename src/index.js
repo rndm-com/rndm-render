5b5a80975393493f67afe37c813c822f6b6b10f4
Paul Napier <>
2018-07-11 14:43:17 +1000

    Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import components, { plugin } from './components';
 import render from './render';
 
-export const use = ({ key, components } = {}) => {
+const use = ({ key, components } = {}) => {
   if(key) {
     if (typeof components === 'object') plugin({ [key]: components })
   }
