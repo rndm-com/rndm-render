7784e6df1f7e6cbeaad1c807d6da0c42c9785d04
Paul Napier <>
2018-07-24 18:12:44 +1000

    Expose build, methods and renderers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import components from './plugins/components';
+import methods from './plugins/methods';
+import renderers from './plugins/renderers';
 import resolve from './plugins/middlewares/resolve';
 import render from './render';
 import use from './use';
+import build from './build';
 
 export {
   use,
+  build,
   components,
+  methods,
+  renderers,
   render,
   resolve,
 };
