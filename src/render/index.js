3294ccd6b8fe1f860de76410e10279fb86d49da9
rndm-com <rndm.dapps@gmail.com>
2018-11-12 17:45:28 +1100

    Version 0.1.1: Improved rendering

--- src/render/index.js
+++ src/render/index.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import { unary, get, stubFalse } from 'lodash';
 import renderers from '../plugins/renderers';
 
 export const promise = (...args) => new Promise(resolve => resolve(render(...args)));
 
-const render = (input, renderer = 'RNDM.core') => (
-  !!input &&
-  (Array.isArray(input) ? input.map(unary(get(renderers, renderer) || stubFalse))
-    : typeof input === 'object' ? (get(renderers, renderer) || stubFalse)(input)
-      : typeof input === 'string' ? input : null)
+const render = (input, renderer = 'RNDM.core', additional) => (
+  input !== undefined &&
+  (
+    Array.isArray(input) ? input.map(unary(get(renderers, renderer)|| stubFalse)) :
+      typeof input === 'object' ? (get(renderers, renderer)|| stubFalse)(input, additional) :
+        typeof input === 'string' ? input :
+          typeof input === 'number' ? `${input}` :
+            typeof input === 'function' ? `${input()}` : null
+  )
 );
 
 render.promise = promise;
